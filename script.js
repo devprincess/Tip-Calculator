b30681056bb14fc48a3883f7cdbc58dbfce8af1b
devprincess <princess.solomon@eng.uniben.edu>
2025-07-08 06:42:40 +0100

    tip calculator

---
+++ script.js
@@ -0,0 +1,25 @@
+const billAmountInput = document.getElementById('bill amount');
+const tipPercentageInput = document.getElementById('tip percentage');
+const calculateButton = document.querySelector('button');
+const totalAmountSpan = document.getElementById('totalAmount');
+
+// console.log(billAmountInput.value);
+
+// get values from input and convert to number
+function calculateTip(){
+    const billAmountValue = Number(billAmountInput.value);
+    const tipPercentageValue = Number(tipPercentageInput.value);
+
+    // calculate the tip amount
+    const tipAmount = (billAmountValue * tipPercentageValue) / 100;
+
+    //calculate the total amount
+    const totalAmount = billAmountValue + tipAmount;
+    
+    //display message
+    totalAmountSpan.innerText = `$${totalAmount.toFixed(2)}`;
+
+}
+
+//cick button to get result
+calculateButton.addEventListener('click', calculateTip);
